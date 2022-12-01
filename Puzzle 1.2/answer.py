4fb93f7fb5b1f39e6178ba52457f6d58d34100e2
Tyler McVety <mcvetyt@vmware.com>
2022-12-01 14:17:16 -0600

    Day 1 Part 2

--- Puzzle 1.2/answer.py
+++ Puzzle 1.2/answer.py
@@ -10,16 +10,20 @@ class Elf:
 def main():
     with open("input.txt") as file:
         current_elf = Elf(0, 1)
-        fat_elf = None
+        top_elfs = [Elf(0, 0), Elf(0, 0), Elf(0, 0)]
+
         for line in file.readlines():
             if line.strip() != "":
                 current_elf.calories += int(line.strip())
             else:
-                if fat_elf is None or fat_elf.calories < current_elf.calories:
-                    fat_elf = current_elf
+                for top in top_elfs:
+                    if current_elf.calories > top.calories:
+                        top_elfs.insert(top_elfs.index(top), current_elf)
+                        top_elfs.pop(-1)
+                        break
                 current_elf = Elf(0, current_elf.elf_id+1)
 
-        return fat_elf.calories
+    return sum([top.calories for top in top_elfs])
 
 
 if __name__ == "__main__":
